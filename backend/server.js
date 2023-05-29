5fccc592c95c001f1556ebe65769d841e21abbbd
Matanel <mstinski@gmail.com>
2023-05-29 20:17:05 +0300

    Deployment changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import imageRoutes from "./routes/imageRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -8,7 +9,19 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+const __dirname = path.resolve();
 app.use("/api/images", imageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 // Middleware for handling 404 errors
 app.use(notFound);
